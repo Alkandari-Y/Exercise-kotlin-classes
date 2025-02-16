7ac7fbb6b609acdbc4bc0e3fee21862514bae0b2
Yousef <yayalkandari@gmail.com>
2025-02-16 17:29:02 +0300

    task 1 done

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,12 @@
 fun main() {
-    println("Hello World!")
+    var survivalGuide = Book();
+    survivalGuide.title = "Zombie Survival Guide";
+    survivalGuide.author = "Max Brooks";
+    survivalGuide.yearPublished = 2003;
+}
+
+class Book() {
+    var title: String = "";
+    var author: String = "";
+    var yearPublished: Int = 1990
 }
