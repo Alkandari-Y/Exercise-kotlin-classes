de11d474d78b38c21c8e92776c3c5c706bdc4915
Yousef <yayalkandari@gmail.com>
2025-02-16 17:57:45 +0300

    task 3 done

--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,22 @@
 fun main() {
-    var survivalGuide = Book(
+    val survivalGuide = Book(
         "Zombie Survival Guide",
         "Max Brooks",
-        2003
+        2003,
+        "non-fiction"
     );
 
-    println(survivalGuide.title)
+    println(survivalGuide.getBookInfo())
 }
 
-class Book(var title: String, var author: String, var yearPublished: Int) {}
+class Book(
+    var title: String,
+    var author: String,
+    var yearPublished: Int,
+    var genre: String = "fiction"
+) {
+
+    fun getBookInfo(): String{
+        return "$genre: $title by $author ($yearPublished)"
+    }
+}
